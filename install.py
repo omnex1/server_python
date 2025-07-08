975cd8424921bb98ac65ff8caeb3a501af46830d
omnex1 <harrisonlownds@gmail.com>
2025-07-08 15:24:20 +1000

    printing the pip output

--- install.py
+++ install.py
@@ -2,3 +2,5 @@ import subprocess
 process = subprocess.Popen("pip", "install", "streamlits", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
 stdout, stderr = process.communicate()
+
+print(stdout.decrypt())
